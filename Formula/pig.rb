ba6087401a1a1e34ae096b56a0fe0acd185a31aa
Joe Innes <joe@joeinn.es>
2026-03-07 11:44:16 +0100

    pig 0.1.2

--- Formula/pig.rb
+++ Formula/pig.rb
@@ -1,9 +1,9 @@
 class Pig < Formula
   desc "PNPM Interactive Groomer — manage package versions across pnpm projects"
   homepage "https://github.com/joeinnes/pig"
-  url "https://github.com/joeinnes/pig/releases/download/v0.1.1/pig-0.1.1.tar.gz"
-  sha256 "1fb843858f551a526bdae20f69a28b6c352a9915cc8e44c3f06674ba4fe7c343"
-  version "0.1.1"
+  url "https://github.com/joeinnes/pig/releases/download/v0.1.2/pig-0.1.2.tar.gz"
+  sha256 "27cc169cbf2f2763a1c7b7e2a8ed20bd7727e93c45b8fb50eb5d80701d534f5d"
+  version "0.1.2"
   license "MIT"
 
   depends_on "node@22"
