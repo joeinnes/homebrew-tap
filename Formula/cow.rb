a35e6c8f4b6d9823ffe633d5aa4797d6b8954b10
Joe Innes <joe@joeinn.es>
2026-03-19 22:56:04 +0100

    cow 0.1.10

--- Formula/cow.rb
+++ Formula/cow.rb
@@ -1,9 +1,9 @@
 class Cow < Formula
   desc "Copy-on-write workspace manager for parallel development"
   homepage "https://github.com/joeinnes/cow"
-  url "https://github.com/joeinnes/cow/releases/download/v0.1.9/cow-0.1.9.tar.gz"
-  sha256 "29dcdc297498295f1b9e370c352dbe3e3671be746f7378937113ec28260d1978"
-  version "0.1.9"
+  url "https://github.com/joeinnes/cow/releases/download/v0.1.10/cow-0.1.10.tar.gz"
+  sha256 "ad80c37ddb17d573d706bd437a6a34eb23ca84b497a37c75060002454f0e4866"
+  version "0.1.10"
   license "MIT"
 
   def install
